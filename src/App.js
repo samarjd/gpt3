0f48deb89e4d800054346c55f6dd110aa9ddb4f9
abc <abc@example.com>
2022-07-30 16:06:57 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Footer, Blog, Possibility, Features, WhatGpt3, Header} from './containers';
+import {Cta, Brand, Navbar} from './componants';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+        <div className='gradient__bg'>
+          <Navbar />
+          <Header />
+        </div>
+        <Brand />
+        <WhatGpt3 />
+        <Features />
+        <Possibility />
+        <Cta />
+        <Blog />
+        <Footer />
+    </div>
+  )
+}
+
+export default App
